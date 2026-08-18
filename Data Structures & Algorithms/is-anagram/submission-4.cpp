12a9ebc9e9360dd7bf79bbb47d2a8c7350fc3cb0
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-18 22:44:04 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>mp;
+        for(int i = 0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        unordered_map<char,int>mp2;
+        for(int i = 0;i<t.size();i++){
+            mp2[t[i]]++;
+        }
+
+        for(int i = 0;i<s.size();i++){
+            if(mp[s[i]]!=mp2[s[i]]) return false;
+        }
+        return true;
+    }
+};
